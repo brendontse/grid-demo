f8c986d00e2388a076b962aa2a4ed3730837b896
unknown <brendonjtse@gmail.com>
2021-03-25 11:45:53 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='logo'>
+        <div className='items'>LOGO</div>
+        <div className='items'>Business Name</div>
+        <div className='items'>Est. 2021</div>
+      </div>
+      <div className='header'>Header</div>
+      <div className='nav'>Nav</div>
+      <div className='content'>Content</div>
+      <div className='footer'>Footer</div>
+    
+    </div>
+  );
+}
+
+export default App;
